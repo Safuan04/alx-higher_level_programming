571633ba47504811998c11729c6f6529d11e18d2
Safuan04 <elmai.safuan@gmail.com>
2023-06-07 17:44:24 -0700

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(c) - 32)
+        print("{}".format(char))

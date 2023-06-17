caf5f418af8e2ce317e0b587dd5c3cf7707ff880
Safuan04 <elmai.safuan@gmail.com>
2023-06-16 17:35:05 -0700

    roman numbers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+        roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
+    result = 0
+
+    for i in range(len(roman_string)):
+        if roman_string[i] not in roman_dict:
+            return None

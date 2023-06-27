1c9c4a4609c46ba07a3a211b2d802766b593082b
Safuan04 <elmai.safuan@gmail.com>
2023-06-27 14:02:15 -0700

    sqaure class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ Define a class"""
 class Square:
+    """ Define a method in a sqaure class"""
     pass
-""" Define a method in a sqaure class"""
-def square(self):
-        pass

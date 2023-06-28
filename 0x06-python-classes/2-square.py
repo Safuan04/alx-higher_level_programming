2f0e14d3eea06db4cb65c21c8f05a7813afdbb45
Safuan04 <elmai.safuan@gmail.com>
2023-06-28 08:24:24 -0700

    square with 2 conditions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Square:
+    """Initializes the size with 2 conditiions,
+        one for the sizes type and the other for the sizes value"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")

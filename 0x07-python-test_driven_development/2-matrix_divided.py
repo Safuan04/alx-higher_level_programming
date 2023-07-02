5648c698a29ff0d8aefe6bc93f634c7cfaae0ad8
Safuan04 <elmai.safuan@gmail.com>
2023-07-02 09:41:16 -0700

    some fixes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
-
 def matrix_divided(matrix, div):
     """divides all elements of a matrix"""
-    new_matrix = matrix.copy()
+    new_matrix = []
 
     if not isinstance(matrix, list) or not all(
             isinstance(row, list) and all(
                 isinstance(element, (int, float)) for element in row)
-            for row in matrix):
+            for row in matrix) or matrix == []:
         raise TypeError("matrix must be a matrix (list of lists) "
                         "of integers/floats")
 
@@ -22,9 +20,10 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if not row_len == len(row):
             raise TypeError("Each row of the matrix must have the same size")
-
-    for i in range(len(new_matrix)):
-        new_matrix[i] = list(
-            map(lambda element: round(element / div, 2), matrix[i]))
+    for i in matrix:
+        new_row = []
+        for element in i:
+            new_row.append(round(element / div, 2))
+        new_matrix.append(new_row)
 
     return new_matrix

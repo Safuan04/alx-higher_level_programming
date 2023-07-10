bd6881de3d9dcc35b572718a35bf432f3bce351a
Safuan04 <elmai.safuan@gmail.com>
2023-07-10 16:20:09 -0700

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining a function named is_same_class"""
+
+
+def is_same_class(obj, a_class):
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False

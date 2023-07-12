ad416fc4101e98791c009c0d4f750e09754221e1
Safuan04 <elmai.safuan@gmail.com>
2023-07-12 04:55:24 -0700

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class called MyInt that inherits from int"""
+
+
+class MyInt(int):
+
+    def __eq__(self, other):
+        """Overrides equality operator."""
+
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Overrides inequality operator."""
+
+        return super().__eq__(other)

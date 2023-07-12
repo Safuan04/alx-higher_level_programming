1463f1e5761cbdc5ca1a2bc5ae1e784d15bfca78
Safuan04 <elmai.safuan@gmail.com>
2023-07-12 04:39:58 -0700

    Square#1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""import a class named Rectangle to inherit from it"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""Define a class named Square that inherits from Rectangle"""
+
+
+class Square(Rectangle):
+    """Instantiation with size"""
+
+    def __init__(self, size):
+        self.__size = size
+
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)

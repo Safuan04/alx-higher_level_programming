28fd33b7ce640a5690843aa0fc1bb8be5c4074e1
Safuan04 <elmai.safuan@gmail.com>
2023-07-12 17:52:13 -0700

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list, and then save them to a file"""
+
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        elements = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        elements = []
+    args = len(sys.argv) - 1
+    for i in range(args):
+        elements.append(sys.argv[i + 1])
+    save_to_json_file(elements, "add_item.json")

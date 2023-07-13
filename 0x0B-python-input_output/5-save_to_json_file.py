50e8adb659de29fb23c6d191a8131a94306ac15a
Safuan04 <elmai.safuan@gmail.com>
2023-07-12 17:25:37 -0700

    Save obj to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining a function called load_from_json_file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """load_from_json_file function creates an Object from a
+    “JSON file”."""
+
+    with open(filename, 'r', encoding="utf-8") as Json_file:
+
+        return json.load(Json_file)

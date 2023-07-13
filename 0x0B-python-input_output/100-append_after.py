150237a5082c49e55a8998a1a42468be47be08de
Safuan04 <elmai.safuan@gmail.com>
2023-07-12 18:24:52 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file."""
+    txt = ""
+    with open(filename) as r:
+        for line in r:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, "w") as w:
+        w.write(txt)

61ff78ee2af38407ad4540515af108306962aec7
Safuan04 <elmai.safuan@gmail.com>
2023-07-12 18:17:09 -0700

    students to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defining a class called Student"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ this methode retrieves a dictionary 
+        representation of a Student instance"""
+
+        if attrs is None:
+            return vars(self)
+
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}

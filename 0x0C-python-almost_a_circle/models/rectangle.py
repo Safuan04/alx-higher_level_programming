c6497d70ed8a6dedf801f2d71e12bd34386c2f93
Safuan04 <elmai.safuan@gmail.com>
2023-07-17 17:38:26 -0700

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,7 @@ class Rectangle(Base):
             for space in range(self.__x):
                 print(" ", end="")
             for j in range(self.__width):
-                print ("#", end="")
+                print("#", end="")
             print()
 
     def __str__(self):

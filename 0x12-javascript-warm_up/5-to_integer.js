17a8517c426fa9a88c31f16778f88d7f01dfb097
Safuan04 <elmail.safuan@gmail.com>
2023-09-11 22:59:04 +0100

    an int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,5 +7,5 @@ const num = parseInt(args[2]);
 if (isNaN(num) || args.length < 3) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', num);
+  console.log('My number:', num);
 }

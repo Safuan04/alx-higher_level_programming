c2166a924733ae72db478ae0181c153919065bb5
Safuan04 <elmail.safuan@gmail.com>
2023-09-17 00:20:20 +0100

    I choose wich state to show if any

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Importing necessary modules"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    server = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    engine = create_engine(server)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name.like(argv[4])).first()
+    if result:
+        print(f"{result.id}")
+    else:
+        print("Not found")

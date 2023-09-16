f70044e203c8faf23e2f18226410f4b908827a94
Safuan04 <elmail.safuan@gmail.com>
2023-09-16 17:36:57 +0100

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""importing MySQLdb to let python connect to a db"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    mycursor = db.cursor()
+
+    mysql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
+    mycursor.execute(mysql)
+
+    result = mycursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    mycursor.close()
+    db.close()

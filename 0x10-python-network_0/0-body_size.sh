9d89e8a614abf90f5e48322f0a469d4a01e1eb0e
Safuan04 <elmail.safuan@gmail.com>
2023-09-29 00:15:21 +0100

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that displays the size of the body of an HTTP response
-curl -Is $1 | grep -i "Content-Length" | awk '{print $2}'
+# Bash script that takes in a URL and displays the size of the body of the response
+curl -Is "$1" | awk -F': ' '/Content-Length/ {print $2}'

c9f430a364d72abbfc45cab6c0c64e8e6c3767dd
Safuan04 <elmail.safuan@gmail.com>
2023-09-30 00:08:18 +0100

    whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,13 @@ import urllib.request
 
 if __name__ == "__main__":
     try:
-        url = 'https://alx-intranet.hbtn.io/status'
-        with urllib.request.urlopen(url) as response:
+        req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+        with urllib.request.urlopen(req) as response:
             the_page = response.read()
 
         print('Body response:')
         print('    - type:', type(the_page))
         print('    - content:', the_page)
-        print('    - utf8 content:', response.reason)
+        print('    - utf8 content:', the_page.decode('utf-8'))
     except urllib.error.URLError as e:
         print(e.reason)

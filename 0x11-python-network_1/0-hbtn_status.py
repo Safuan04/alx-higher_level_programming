843475ddf05a30b331e4bbd8f2b3500300bd37aa
Safuan04 <elmail.safuan@gmail.com>
2023-09-30 00:01:46 +0100

    whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,14 @@
 if __name__ == "__main__":
 
     import urllib.request
+    try:
+        req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read()
 
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-
-    print('Body response:')
-    print('    - type:', type(the_page))
-    print('    - content:', the_page)
-    print('    - utf8 content:', response.reason)
+        print('Body response:')
+        print('    - type:', type(the_page))
+        print('    - content:', the_page)
+        print('    - utf8 content:', response.reason)
+    except urllib.error.URLError as e:
+        print(e.reason)

94362f40ce04acdfbf89d9d226fb69ce2a636852
Safuan04 <elmail.safuan@gmail.com>
2023-09-29 01:28:17 +0100

    allowed methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -ls "$1" | awk -F': ' '/^Allow/ {print $2}'
+curl -ls "$1" | awk -F ': ' '/^Allow:/ {print $2}'

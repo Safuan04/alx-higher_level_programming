6ce522dc6c391824a486e3031c6bbe2ba9b35c3e
Safuan04 <elmail.safuan@gmail.com>
2023-09-30 00:26:53 +0100

    Full SEND Part 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,5 @@
-    
 #!/usr/bin/python3
-"""Python script that sends a POST request to the passed URL
+"""Python script that sends a POST request to the passed URL 
 with an email as parameter"""
 
 import urllib.request
@@ -9,11 +8,10 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    emails = {'email': argv[2]}
-    data = parse.urlencode(emails).encode("utf-8")
-    req = request.Request(argv[1], data=data, method="POST")
-
     try:
+        emails = {'email': argv[2]}
+        data = parse.urlencode(emails).encode("utf-8")
+        req = request.Request(argv[1], data=data, method="POST")
         with request.urlopen(req) as resp:
             resp_data = resp.read().decode("utf-8")
             print(resp_data)

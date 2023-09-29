28c1d23909c441499f228200540cb88283ceb4ee
Safuan04 <elmail.safuan@gmail.com>
2023-09-29 12:32:57 +0100

    status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -Is "$1"  awk -F ' ' '/'HTTP/'/ {print $2}'
+curl -sw "%{http_code}" "$1" -o /dev/null
+

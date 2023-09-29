511c0dbfc889496f6815189299208927942d6477
Safuan04 <elmail.safuan@gmail.com>
2023-09-29 23:55:59 +0100

    whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """importing necessary moduls"""
 
-import urllib.request
+if __name__ == "__main__":
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
+    import urllib.request
 
-print('Body response:')
-print('    - type:', type(the_page))
-print('    - content:', the_page)
-print('    - utf8 content:', response.reason)
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+
+    print('Body response:')
+    print('    - type:', type(the_page))
+    print('    - content:', the_page)
+    print('    - utf8 content:', response.reason)

77be4ad4026489ad98b15664d33d622cdedd9716
Safuan04 <elmail.safuan@gmail.com>
2023-09-30 16:16:08 +0100

    Post email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+from sys import argv
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+
+if __name__ == "__name__":
+
+    url = argv[1]
+    email = argv[2]
+    url_email = urlencode(email)
+
+    with urlopen(url, "/", email) as response:
+        the_page = response.read()
+
+    print(the_page.decode('URF-8'))

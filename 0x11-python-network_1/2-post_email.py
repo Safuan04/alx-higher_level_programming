f1a335029de45c78f1b7e074d5e88ce955101b57
Safuan04 <elmail.safuan@gmail.com>
2023-09-30 16:23:43 +0100

    Post email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,8 +12,9 @@ if __name__ == "__name__":
     url = argv[1]
     email = argv[2]
     url_email = urlencode({'email': email})
+    req = Request(url)
 
-    with urlopen(url, email) as response:
+    with urlopen(url, url_email) as response:
         the_page = response.read()
 
     print(the_page.decode('URF-8'))

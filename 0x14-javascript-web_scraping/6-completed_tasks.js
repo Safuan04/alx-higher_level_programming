37af00d532691386a2f69f39cf05428a4b42ea0c
Safuan04 <elmail.safuan@gmail.com>
2023-10-25 01:39:02 +0100

    Count complete

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+const argv = process.argv;
+
+
+request(argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const usersTasks = {};
+    const todosData = JSON.parse(body);
+    todosData.forEach((item) => {
+      if (item.completed === true) {
+        const usersID = item.userId;
+        if (!(usersID in usersTasks)) {
+          usersTasks[usersID] = 0;
+        }
+        usersTasks[usersID] += 1;
+      }
+    });
+    console.log(usersTasks);
+  }
+});
